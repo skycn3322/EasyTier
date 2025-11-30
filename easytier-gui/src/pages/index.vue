c880c923ccf47cd52a36dbd2e0778e6192e49518
unknown <9385966@gmail.com>
2025-11-30 22:03:53 +0800

    源码提交

---
+++ easytier-gui/src/pages/index.vue
@@ -0,0 +1,392 @@
+<script setup lang="ts">
+
+import { type } from '@tauri-apps/plugin-os'
+
+import { appLogDir } from '@tauri-apps/api/path'
+import { writeText } from '@tauri-apps/plugin-clipboard-manager'
+import { exit } from '@tauri-apps/plugin-process'
+import { I18nUtils, RemoteManagement } from "easytier-frontend-lib"
+import type { MenuItem } from 'primevue/menuitem'
+import { useTray } from '~/composables/tray'
+import { GUIRemoteClient } from '~/modules/api'
+
+import { getDockVisibilityStatus, loadDockVisibilityAsync } from '~/modules/dock_visibility'
+import { useToast, useConfirm } from 'primevue'
+import { loadMode, saveMode, type Mode } from '~/composables/mode'
+import ModeSwitcher from '~/components/ModeSwitcher.vue'
+import { getServiceStatus, type ServiceStatus } from '~/composables/backend'
+
+const { t, locale } = useI18n()
+const confirm = useConfirm()
+const aboutVisible = ref(false)
+const modeDialogVisible = ref(false)
+const currentMode = ref<Mode>({ mode: 'normal' })
+const editingMode = ref<Mode>({ mode: 'normal' })
+const isModeSaving = ref(false)
+const serviceStatus = ref<ServiceStatus>('NotInstalled')
+
+async function openModeDialog() {
+  editingMode.value = JSON.parse(JSON.stringify(loadMode()))
+  if (editingMode.value.mode === 'service') {
+    serviceStatus.value = await getServiceStatus()
+  }
+  modeDialogVisible.value = true
+}
+
+async function onModeSave() {
+  if (isModeSaving.value) {
+    return;
+  }
+  isModeSaving.value = true
+  try {
+    await initWithMode(editingMode.value);
+    modeDialogVisible.value = false
+  }
+  catch (e: any) {
+    toast.add({ severity: 'error', summary: t('error'), detail: e, life: 10000 })
+    console.error("Error switching mode", e, currentMode.value, editingMode.value)
+    await initWithMode(currentMode.value);
+  }
+  finally {
+    isModeSaving.value = false
+  }
+}
+
+async function onUninstallService() {
+  confirm.require({
+    message: t('mode.uninstall_service_confirm'),
+    header: t('mode.uninstall_service'),
+    icon: 'pi pi-exclamation-triangle',
+    rejectProps: {
+      label: t('web.common.cancel'),
+      severity: 'secondary',
+      outlined: true
+    },
+    acceptProps: {
+      label: t('mode.uninstall_service'),
+      severity: 'danger'
+    },
+    accept: async () => {
+      isModeSaving.value = true
+      try {
+        await initWithMode({ ...currentMode.value, mode: 'normal' });
+        await initService(undefined)
+        toast.add({ severity: 'success', summary: t('web.common.success'), detail: t('mode.uninstall_service_success'), life: 3000 })
+        modeDialogVisible.value = false
+      } catch (e: any) {
+        toast.add({ severity: 'error', summary: t('error'), detail: e, life: 10000 })
+        console.error("Error uninstalling service", e)
+      } finally {
+        isModeSaving.value = false
+      }
+    },
+  });
+}
+
+async function onStopService() {
+  isModeSaving.value = true
+  try {
+    await setServiceStatus(false)
+    toast.add({ severity: 'success', summary: t('web.common.success'), detail: t('mode.stop_service_success'), life: 3000 })
+    modeDialogVisible.value = false
+  }
+  catch (e: any) {
+    toast.add({ severity: 'error', summary: t('error'), detail: e, life: 10000 })
+    console.error("Error stopping service", e)
+  }
+  finally {
+    isModeSaving.value = false
+  }
+}
+
+async function initWithMode(mode: Mode) {
+  if (currentMode.value.mode === 'service' && mode.mode !== 'service') {
+    let serviceStatus = await getServiceStatus()
+    if (serviceStatus === "Running") {
+      await setServiceStatus(false)
+      serviceStatus = await getServiceStatus()
+    }
+    if (serviceStatus === "Stopped") {
+      await initService(undefined)
+    }
+  }
+
+  let url: string | undefined = undefined
+  let retrys = 1
+  switch (mode.mode) {
+    case 'remote':
+      if (!mode.remote_rpc_address) {
+        toast.add({ severity: 'error', summary: t('error'), detail: t('mode.remote_rpc_address_empty'), life: 10000 })
+        return initWithMode({ ...mode, mode: 'normal' });
+      }
+      url = mode.remote_rpc_address
+      break;
+    case 'service':
+      if (!mode.config_dir || !mode.file_log_dir || !mode.file_log_level || !mode.rpc_portal) {
+        toast.add({ severity: 'error', summary: t('error'), detail: t('mode.service_config_empty'), life: 10000 })
+        return initWithMode({ ...mode, mode: 'normal' });
+      }
+      let serviceStatus = await getServiceStatus()
+      if (serviceStatus === "NotInstalled" || JSON.stringify(mode) !== JSON.stringify(currentMode.value)) {
+        await initService({
+          config_dir: mode.config_dir,
+          file_log_dir: mode.file_log_dir,
+          file_log_level: mode.file_log_level,
+          rpc_portal: mode.rpc_portal,
+        })
+        serviceStatus = await getServiceStatus()
+      }
+      if (serviceStatus === "Stopped") {
+        await setServiceStatus(true)
+      }
+      url = "tcp://" + mode.rpc_portal.replace("0.0.0.0", "127.0.0.1")
+      retrys = 5
+      break;
+  }
+  for (let i = 0; i < retrys; i++) {
+    try {
+      await connectRpcClient(url)
+      break;
+    } catch (e) {
+      if (i === retrys - 1) {
+        throw e;
+      }
+      console.error("Error connecting rpc client, retrying...", e)
+      await new Promise(resolve => setTimeout(resolve, 1000))
+    }
+  }
+  currentMode.value = mode
+  saveMode(mode)
+  clientRunning.value = await isClientRunning()
+}
+
+onMounted(() => {
+  currentMode.value = loadMode()
+  initWithMode(currentMode.value);
+});
+
+useTray(true)
+let toast = useToast();
+
+const remoteClient = computed(() => new GUIRemoteClient());
+const instanceId = ref<string | undefined>(undefined);
+const clientRunning = ref(false);
+
+watch(clientRunning, async (newVal, oldVal) => {
+  if (!newVal && oldVal) {
+    await reconnectClient()
+  }
+})
+
+onMounted(async () => {
+  clientRunning.value = await isClientRunning().catch(() => false)
+  const timer = setInterval(async () => {
+    try {
+      clientRunning.value = await isClientRunning()
+    } catch (e) {
+      clientRunning.value = false
+      console.error("Error checking client running status", e)
+    }
+  }, 1000)
+  return () => {
+    clearInterval(timer)
+  }
+})
+async function reconnectClient() {
+  editingMode.value = JSON.parse(JSON.stringify(loadMode()));
+  await onModeSave()
+}
+
+onMounted(async () => {
+  window.setTimeout(async () => {
+    await setTrayMenu([
+      await MenuItemShow(t('tray.show')),
+      await MenuItemExit(t('tray.exit')),
+    ])
+  }, 1000)
+})
+
+let current_log_level = 'off'
+
+const log_menu = ref()
+const log_menu_items_popup: Ref<MenuItem[]> = ref([
+  ...['off', 'warn', 'info', 'debug', 'trace'].map(level => ({
+    label: () => t(`logging_level_${level}`) + (current_log_level === level ? ' ✓' : ''),
+    command: async () => {
+      current_log_level = level
+      await setLoggingLevel(level)
+    },
+  })),
+  {
+    separator: true,
+  },
+  {
+    label: () => t('logging_open_dir'),
+    icon: 'pi pi-folder-open',
+    command: async () => {
+      // console.log('open log dir', await appLogDir())
+      await open(await appLogDir())
+    },
+  },
+  {
+    label: () => t('logging_copy_dir'),
+    icon: 'pi pi-tablet',
+    command: async () => {
+      await writeText(await appLogDir())
+    },
+  },
+])
+
+function toggle_log_menu(event: any) {
+  log_menu.value.toggle(event)
+}
+
+function getLabel(item: MenuItem) {
+  return typeof item.label === 'function' ? item.label() : item.label
+}
+
+const setting_menu_items: Ref<MenuItem[]> = ref([
+  {
+    label: () => t('exchange_language'),
+    icon: 'pi pi-language',
+    command: async () => {
+      await I18nUtils.loadLanguageAsync((locale.value === 'en' ? 'cn' : 'en'))
+      await setTrayMenu([
+        await MenuItemShow(t('tray.show')),
+        await MenuItemExit(t('tray.exit')),
+      ])
+    },
+  },
+  {
+    label: () => `${t('mode.switch_mode')}: ${t('mode.' + currentMode.value.mode)}`,
+    icon: 'pi pi-sync',
+    command: openModeDialog,
+    visible: () => type() !== 'android',
+  },
+  {
+    label: () => getDockVisibilityStatus() ? t('hide_dock_icon') : t('show_dock_icon'),
+    icon: 'pi pi-eye-slash',
+    command: async () => {
+      await loadDockVisibilityAsync(!getDockVisibilityStatus())
+    },
+    visible: () => type() === 'macos',
+  },
+  {
+    key: 'logging_menu',
+    label: () => t('logging'),
+    icon: 'pi pi-file',
+    items: [], // Keep this to show it's a parent menu
+  },
+  {
+    label: () => t('about.title'),
+    icon: 'pi pi-at',
+    command: async () => {
+      aboutVisible.value = true
+    },
+  },
+  {
+    label: () => t('exit'),
+    icon: 'pi pi-power-off',
+    command: async () => {
+      await exit(1)
+    },
+  },
+])
+
+async function connectRpcClient(url?: string) {
+  await initRpcConnection(url)
+  await sendConfigs()
+  console.log("easytier rpc connection established")
+}
+
+onMounted(async () => {
+  if (type() === 'android') {
+    try {
+      await initMobileVpnService()
+      console.error("easytier init vpn service done")
+    } catch (e: any) {
+      console.error("easytier init vpn service failed", e)
+    }
+  }
+  const unlisten = await listenGlobalEvents()
+  return () => {
+    unlisten()
+  }
+})
+
+</script>
+
+<template>
+  <div id="root" class="flex flex-col">
+    <Dialog v-model:visible="aboutVisible" modal :header="t('about.title')" :style="{ width: '70%' }">
+      <About />
+    </Dialog>
+    <Dialog v-model:visible="modeDialogVisible" modal :header="t('mode.switch_mode')" :style="{ width: '50vw' }">
+      <ModeSwitcher v-model="editingMode" @uninstall-service="onUninstallService" @stop-service="onStopService" />
+      <template #footer>
+        <Button :label="t('web.common.cancel')" icon="pi pi-times" @click="modeDialogVisible = false" text />
+        <Button :label="t('web.common.save')" icon="pi pi-save" @click="onModeSave" autofocus :loading="isModeSaving" />
+      </template>
+    </Dialog>
+    <Menu ref="log_menu" :model="log_menu_items_popup" :popup="true" />
+
+    <RemoteManagement v-if="clientRunning" class="flex-1 overflow-y-auto" :api="remoteClient"
+      :pause-auto-refresh="isModeSaving" v-bind:instance-id="instanceId" />
+    <div v-else class="empty-state flex-1 flex flex-col items-center py-12">
+      <i class="pi pi-server text-5xl text-secondary mb-4 opacity-50"></i>
+      <div class="text-xl text-center font-medium mb-3">{{ t('client.not_running') }}
+      </div>
+      <Button @click="reconnectClient" :loading="isModeSaving" :label="t('client.retry')" icon="pi pi-replay"
+        iconPos="left" />
+    </div>
+
+    <Menubar :model="setting_menu_items" breakpoint="560px">
+      <template #item="{ item, props }">
+        <a v-if="item.key === 'logging_menu'" v-bind="props.action" @click="toggle_log_menu">
+          <span :class="item.icon" />
+          <span class="p-menubar-item-label">{{ getLabel(item) }}</span>
+          <span class="pi pi-angle-down p-menubar-item-icon text-[9px]"></span>
+        </a>
+        <a v-else v-bind="props.action">
+          <span :class="item.icon" />
+          <span class="p-menubar-item-label">{{ getLabel(item) }}</span>
+        </a>
+      </template>
+    </Menubar>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+#root {
+  height: 100vh;
+  width: 100vw;
+}
+
+.p-dropdown :deep(.p-dropdown-panel .p-dropdown-items .p-dropdown-item) {
+  padding: 0 0.5rem;
+}
+</style>
+
+<style>
+body {
+  height: 100vh;
+  width: 100vw;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
+
+.p-menubar .p-menuitem {
+  margin: 0;
+}
+
+.p-select-overlay {
+  max-width: calc(100% - 2rem);
+}
+
+/*
+
+.p-tabview-panel {
+  height: 100%;
+} */
+</style>
---
+++ easytier-web/src/restful/auth.rs
@@ -0,0 +1,188 @@
+use axum::{
+    http::StatusCode,
+    routing::{get, post, put},
+    Router,
+};
+use axum_login::login_required;
+use axum_messages::Message;
+use serde::{Deserialize, Serialize};
+
+use crate::restful::users::Backend;
+
+use super::{
+    users::{AuthSession, Credentials},
+    AppStateInner,
+};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct LoginResult {
+    messages: Vec<Message>,
+}
+
+pub fn router() -> Router<AppStateInner> {
+    let r = Router::new()
+        .route("/api/v1/auth/password", put(self::put::change_password))
+        .route(
+            "/api/v1/auth/check_login_status",
+            get(self::get::check_login_status),
+        )
+        .route_layer(login_required!(Backend));
+    Router::new()
+        .merge(r)
+        .route("/api/v1/auth/login", post(self::post::login))
+        .route("/api/v1/auth/logout", get(self::get::logout))
+        .route("/api/v1/auth/captcha", get(self::get::get_captcha))
+        .route("/api/v1/auth/register", post(self::post::register))
+}
+
+mod put {
+    use axum::Json;
+    use axum_login::AuthUser;
+    use easytier::proto::common::Void;
+
+    use crate::restful::{other_error, users::ChangePassword, HttpHandleError};
+
+    use super::*;
+
+    pub async fn change_password(
+        mut auth_session: AuthSession,
+        Json(req): Json<ChangePassword>,
+    ) -> Result<Json<Void>, HttpHandleError> {
+        if let Err(e) = auth_session
+            .backend
+            .change_password(auth_session.user.as_ref().unwrap().id(), &req)
+            .await
+        {
+            tracing::error!("修改密码失败: {:?}", e);
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json::from(other_error(format!("修改密码失败: {:?}", e))),
+            ));
+        }
+
+        let _ = auth_session.logout().await;
+
+        Ok(Void::default().into())
+    }
+}
+
+mod post {
+    use axum::Json;
+    use easytier::proto::common::Void;
+
+    use crate::restful::{
+        captcha::extension::{axum_tower_sessions::CaptchaAxumTowerSessionStaticExt, CaptchaUtil},
+        other_error,
+        users::RegisterNewUser,
+        HttpHandleError,
+    };
+
+    use super::*;
+
+    pub async fn login(
+        mut auth_session: AuthSession,
+        Json(creds): Json<Credentials>,
+    ) -> Result<Json<Void>, HttpHandleError> {
+        let user = match auth_session.authenticate(creds.clone()).await {
+            Ok(Some(user)) => user,
+            Ok(None) => {
+                return Err((
+                    StatusCode::UNAUTHORIZED,
+                    Json::from(other_error("用户名或密码错误")),
+                ));
+            }
+            Err(e) => {
+                return Err((
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json::from(other_error(format!("服务器内部错误: {:?}", e))),
+                ))
+            }
+        };
+
+        if let Err(e) = auth_session.login(&user).await {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json::from(other_error(format!("登录失败: {:?}", e))),
+            ));
+        }
+
+        Ok(Void::default().into())
+    }
+
+    pub async fn register(
+        auth_session: AuthSession,
+        captcha_session: tower_sessions::Session,
+        Json(req): Json<RegisterNewUser>,
+    ) -> Result<Json<Void>, HttpHandleError> {
+        // 调用CaptchaUtil的静态方法验证验证码是否正确
+        if !CaptchaUtil::ver(&req.captcha, &captcha_session).await {
+            return Err((
+                StatusCode::BAD_REQUEST,
+                other_error(format!("验证码错误，输入值: {}", req.captcha)).into(),
+            ));
+        }
+
+        if let Err(e) = auth_session.backend.register_new_user(&req).await {
+            tracing::error!("注册失败，本站已关闭注册");
+            return Err((
+                StatusCode::BAD_REQUEST,
+                other_error("禁止注册"),
+            ));
+        }
+
+        Ok(Void::default().into())
+    }
+}
+
+mod get {
+    use crate::restful::{
+        captcha::{
+            builder::spec::SpecCaptcha,
+            extension::{axum_tower_sessions::CaptchaAxumTowerSessionExt as _, CaptchaUtil},
+            NewCaptcha as _,
+        },
+        other_error, HttpHandleError,
+    };
+    use axum::{response::Response, Json};
+    use easytier::proto::common::Void;
+    use tower_sessions::Session;
+
+    use super::*;
+
+    pub async fn logout(mut auth_session: AuthSession) -> Result<Json<Void>, HttpHandleError> {
+        match auth_session.logout().await {
+            Ok(_) => Ok(Json(Void::default())),
+            Err(e) => {
+                tracing::error!("退出登录失败: {:?}", e);
+                Err((
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json::from(other_error(format!("退出登录失败: {:?}", e))),
+                ))
+            }
+        }
+    }
+
+    pub async fn get_captcha(session: Session) -> Result<Response, HttpHandleError> {
+        let mut captcha: CaptchaUtil<SpecCaptcha> = CaptchaUtil::with_size_and_len(127, 48, 4);
+        match captcha.out(&session).await {
+            Ok(response) => Ok(response),
+            Err(e) => Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json::from(other_error(format!("生成验证码失败: {:?}", e))),
+            )),
+        }
+    }
+
+    pub async fn check_login_status(
+        auth_session: AuthSession,
+    ) -> Result<Json<Void>, HttpHandleError> {
+        if auth_session.user.is_some() {
+            Ok(Json(Void::default()))
+        } else {
+            Err((
+                StatusCode::UNAUTHORIZED,
+                Json::from(other_error("未登录")),
+            ))
+        }
+    }
+}
